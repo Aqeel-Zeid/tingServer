0cff58d3644f8d07305d7ed7c0994a0b622c8df6
Mohamed Rifan <mohamedrifan@live.com>
2020-10-17 21:56:16 +0530

    Initialized Spring Application with Spring Data JPA and MySql

---
+++ tingServer/tingServer/src/main/java/com/ting/tingServer/TingServerApplication.java
@@ -0,0 +1,13 @@
+package com.ting.tingServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TingServerApplication.class, args);
+	}
+
+}
---
+++ tingServer/tingServer/src/test/java/com/ting/tingServer/TingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ting.tingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
